ee910bb808d104883a409d9bc79e4200acab1e8b
JulianNavi <39164188+JulianNavi@users.noreply.github.com>
2019-07-07 18:36:08 +0300

    added desing material

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Button = ({ buttonDisplayName, clickHandler }) => (
-  // eslint-disable-next-line no-unused-expressions
-  <button onClick={clickHandler}>{buttonDisplayName}</button>
-);
-
-export default Button;
